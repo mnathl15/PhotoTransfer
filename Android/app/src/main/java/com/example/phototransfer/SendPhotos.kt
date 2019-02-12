876fa83c638043009aa75431134bbae8d6ad4858
unknown <myrealactualemail@gmail.com>
2019-02-12 17:09:09 -0500

    Third commit

--- Android/app/src/main/java/com/example/phototransfer/SendPhotos.kt
+++ Android/app/src/main/java/com/example/phototransfer/SendPhotos.kt
@@ -4,22 +4,29 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_send_photos.*
 
 import kotlinx.android.synthetic.main.activity_send_photos.*
 import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.lang.Exception
 import java.net.URI
+import java.util.*
 
 class SendPhotos : AppCompatActivity() {
 
@@ -28,12 +35,15 @@ class SendPhotos : AppCompatActivity() {
     private lateinit var socket: BluetoothSocket
     private lateinit var clientThread: ClientThread
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_photos)
 
 
         device = intent.extras.get("Device")
+
+
 
 
         //makeDiscoverable()
@@ -64,7 +74,7 @@ class SendPhotos : AppCompatActivity() {
 
         try{
 
-            clientThread.run()
+            clientThread.execute()
             socket = clientThread.mmSocket
 
         }
@@ -93,16 +103,19 @@ class SendPhotos : AppCompatActivity() {
 
 
             try{
+
                 currentBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(targetUri))
                 image.setImageBitmap(currentBitmap)
 
-                val stream:ByteArrayOutputStream = ByteArrayOutputStream()
+                val stream = ByteArrayOutputStream()
                 currentBitmap.compress(Bitmap.CompressFormat.PNG,100,stream)
 
                 val byteArray:ByteArray = stream.toByteArray()
 
 
-                sendPhoto(byteArray)
+                Log.d("TAG","THE SIZE IS: " + byteArray.size)
+
+                clientThread.write(byteArray)
 
 
             }
@@ -117,14 +130,89 @@ class SendPhotos : AppCompatActivity() {
 
 
 
-    private fun sendPhoto(byteArray: ByteArray) {
 
 
-        clientThread.write(byteArray)
+
+    inner class ClientThread(device: BluetoothDevice,bluetoothAdapter: BluetoothAdapter?) : AsyncTask<Void, Void, Boolean>() {
+
+        val device = device
+        val bluetoothAdapter = bluetoothAdapter
+
+        private val PORT_NUMBER = 25
+
+        private val TIMEOUTNUM = 100
+        private lateinit var output: OutputStream
+        private lateinit var fallbackSocket: BluetoothSocket
+        private var uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+
+        override fun doInBackground(vararg p0: Void?): Boolean {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+            var socket = device.createInsecureRfcommSocketToServiceRecord(uuid)
+
+            var clazz = socket.remoteDevice.javaClass
+            var paramTypes = arrayOf<Class<*>>(Integer.TYPE)
+            var m = clazz.getMethod("createRfcommSocket", *paramTypes)
+            fallbackSocket = m.invoke(socket.remoteDevice, Integer.valueOf(PORT_NUMBER)) as BluetoothSocket
+
+            var startTime = Date().time
+            var timeElapsed: Long = 0
+            while (timeElapsed < TIMEOUTNUM) {
+                try {
+                    fallbackSocket.connect()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                publishProgress()
+
+                timeElapsed = Date().time - startTime
+
+            }
+
+
+
+
+            return fallbackSocket.isConnected
+        }
+
+        override fun onProgressUpdate(vararg values: Void?) {
+            super.onProgressUpdate(*values)
+            error.setText("Trying to connect!")
+        }
+
+
+
+        override fun onPostExecute(result: Boolean) {
+            loading.visibility = View.GONE
+            if(result){
+                error.setText("Connection successful")
+            }else{
+                error.setText("Could not connect")
+            }
+            Toast.makeText(applicationContext,"DONEEE",Toast.LENGTH_LONG).show()
+        }
+
+
+        val mmSocket: BluetoothSocket by lazy(LazyThreadSafetyMode.NONE) {
+
+            device.createRfcommSocketToServiceRecord(uuid)
+        }
+
+
+        fun write(bytes: ByteArray) {
+            output = fallbackSocket.outputStream
+            Log.d("TAG","HERE ARE YOUR BYTES: " + bytes)
+            output.write(bytes)
+
+
+        }
+
 
 
 
 
     }
+
 
 }
--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,11 @@ import bluetooth
 
 HOST = ''          # Symbolic name
 PORT = 25
+import os
+import io
+from PIL import Image
+from PIL import ImageFile
+
 
 # print ("performing inquiry...")
 #
@@ -27,15 +32,26 @@ server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 server_sock.bind(("",PORT))
 server_sock.listen(PORT)
 
-
 client_sock,address = server_sock.accept()
 print ("Accepted connection from ",address)
 
-data = client_sock.recv(1024)
-print ("received [%s]" % data)
+
+data = client_sock.recv(100000)
+
+
+print("received [%s]" % data)
+
+print(len(data))
 
 
 
+
+with open("C:/Users/Michael/Desktop/image.png", "wb") as f:
+     f.write(data)
+
+# orginal_image = Image.open(data)
+#
+# orginal_image.save("C:/Users/Michael/Desktop/image.png")
 
 
 client_sock.close()
